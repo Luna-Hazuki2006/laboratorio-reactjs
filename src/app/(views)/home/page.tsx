cc5ec5db12aa16011680ee320973430efc54412a
Luna Hazuki <lunahazuki2006@outlook.com>
2025-05-29 11:05:50 -0400

    Arreglo con el filtrar visualmente hablando

--- src/app/(views)/home/page.tsx
+++ src/app/(views)/home/page.tsx
@@ -5,71 +5,72 @@ import { useRouter } from 'next/navigation';
 import { IArticle } from '@/types/article';
 
 export default function Home() {
-  const [articles, setArticles] = useState<IArticle[]>([]);
-  const [filtered, setFiltered] = useState<IArticle[]>([]);
-  const [filterCategory, setFilterCategory] = useState('');
-  const [filterDate, setFilterDate] = useState('');
-  const router = useRouter();
+    const [articles, setArticles] = useState<IArticle[]>([]);
+    const [filtered, setFiltered] = useState<IArticle[]>([]);
+    const [filterCategory, setFilterCategory] = useState('');
+    const [filterDate, setFilterDate] = useState('');
+    const router = useRouter();
 
-  const fetchArticles = async () => {
-      try {
-        const res = await fetch('/api/articles');
-        const data = await res.json();
-        setArticles(data.data);
-        setFiltered(data.data);
-      } catch (error) {
-        console.error('Error al obtener artículos:', error);
-      }
+    const fetchArticles = async () => {
+        try {
+            const res = await fetch('/api/articles');
+            const data = await res.json();
+            setArticles(data.data);
+            setFiltered(data.data);
+        } catch (error) {
+            console.error('Error al obtener artículos:', error);
+        }
     };
 
-  useEffect(() => {
-    fetchArticles();
-  }, []);
+    useEffect(() => {
+        fetchArticles();
+    }, []);
 
-  const handleFilter = () => {
-    const filteredArticles = articles.filter(article =>
-      article.category.toLowerCase().includes(filterCategory.toLowerCase())
-      && article.date.includes(filterDate)
+    const handleFilter = () => {
+        const filteredArticles = articles.filter(article =>
+            article.category.toLowerCase().includes(filterCategory.toLowerCase())
+            && article.date.includes(filterDate)
+        );
+        setFiltered(filteredArticles);
+    };
+
+    //watch los filtros
+    useEffect(() => {
+        handleFilter();
+    }, [filterCategory, filterDate]);
+
+    const handleClickArticle = (id: string) => {
+        router.push(`/article/${id}`);
+    };
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <input
+            className='mb-3'
+                type="text"
+                placeholder="Filtrar por categoría"
+                value={filterCategory}
+                onChange={(e) => setFilterCategory(e.target.value)}
+            />
+
+            <input
+            className='mb-3'
+                type="date"
+                placeholder="Filtrar por fecha"
+                value={filterDate}
+                onChange={(e) => setFilterDate(e.target.value)}
+            />
+
+            <ul>
+                {filtered.map(article => (
+                    <li key={article._id}>
+                        <h2>{article.title}</h2>
+                        <p>{article.category}</p>
+                        <button onClick={() => handleClickArticle(article._id)}>Leer artículo</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-    setFiltered(filteredArticles);
-  };
-
-  //watch los filtros
-  useEffect(() => {
-    handleFilter();
-  }, [filterCategory, filterDate]);
-
-  const handleClickArticle = (id: string) => {
-    router.push(`/article/${id}`);
-  };
-
-  return (
-    <div>
-      <h1>Home</h1>
-
-      <input
-        type="text"
-        placeholder="Filtrar por categoría"
-        value={filterCategory}
-        onChange={(e) => setFilterCategory(e.target.value)}
-      />
-
-      <input
-        type="date"
-        placeholder="Filtrar por fecha"
-        value={filterDate}
-        onChange={(e) => setFilterDate(e.target.value)}
-      />
-
-      <ul>
-        {filtered.map(article => (
-          <li key={article._id}>
-            <h2>{article.title}</h2>
-            <p>{article.category}</p>
-            <button onClick={() => handleClickArticle(article._id)}>Leer artículo</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
